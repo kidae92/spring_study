8b7d37b5691402609ff537c1ca4fb51b759a53fa
kidae92 <kidae92@hanmail.net>
2022-06-13 19:18:11 +0900

    .

---
+++ src/main/java/Gidae/study/trace/TraceId.java
@@ -0,0 +1,17 @@
+package Gidae.study.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0,8);
+    }
+}
