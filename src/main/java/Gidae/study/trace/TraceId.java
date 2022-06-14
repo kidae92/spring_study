1ae086ee09b1b661993de82c959d8cafbb5d4dfb
kidae92 <kidae92@hanmail.net>
2022-06-14 13:15:14 +0900

    TraceId

--- src/main/java/Gidae/study/trace/TraceId.java
+++ src/main/java/Gidae/study/trace/TraceId.java
@@ -3,6 +3,7 @@ package Gidae.study.trace;
 import java.util.UUID;
 
 public class TraceId {
+
     private String id;
     private int level;
 
@@ -11,7 +12,31 @@ public class TraceId {
         this.level = 0;
     }
 
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
     private String createId() {
         return UUID.randomUUID().toString().substring(0,8);
+    }
+
+    public TraceId createNextId(){
+        return new TraceId(id, level+1);
+    }
+
+    public TraceId createPreviousId(){
+        return new TraceId(id, level + 1);
+    }
+    public boolean ifFirstLevel(){
+        return level == 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
     }
 }
